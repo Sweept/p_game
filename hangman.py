ecc0da297c762bd4a44347e978c4401c8ada9184
Sweept <emersonsridhar@gmail.com>
2020-08-04 02:29:44 -0700

    Game works, bug at the end when displaying hangman lost board

--- hangman.py
+++ hangman.py
@@ -41,7 +41,7 @@ words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote cr
 def getRandomWord(wordList):
     wordList_Split = wordList.split()
     word_index = random.randint(0, len(wordList_Split) - 1)
-    return wordList_Split(word_index)
+    return wordList_Split[word_index]
 
 
 def displayBoard(missedLetters, correctLetters, secretWord):
@@ -55,7 +55,7 @@ def displayBoard(missedLetters, correctLetters, secretWord):
     blanks = '_' * len(secretWord)
 
     for i in range(len(secretWord)):
-        if secretWord(i) in correctLetters:
+        if secretWord[i] in correctLetters:
             # Trick is here
             blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
 
@@ -99,4 +99,28 @@ while True:
         correctLetters = correctLetters + guess
         # Check if the player has won
         foundAllLetters = True
-        for i in range()
+        for i in range(len(secretWord)):
+            if secretWord[i] not in correctLetters:
+                foundAllLetters = False
+                break
+        if foundAllLetters:
+            print("Correct, the word is {}. You've won!".format(secretWord))
+            gameIsDone = True
+    else:
+        missedLetters = missedLetters + guess
+        # Check if player has guessed too many times and lost
+        if len(missedLetters) == len(HANGMAN_PICS) - 1:
+            displayBoard(missedLetters, correctLetters, secretWord)
+            print("You have run out of guesses!\nAfter {} missed guesses and {} correct guesses, the word was {}".format(
+                str(len(missedLetters)), str(len(correctLetters)), secretWord))
+            gameIsDone = True
+
+# Ask the player if they want to play again if the game is ended
+if gameIsDone:
+    if playAgain():
+        missedLetters = ''
+        correctLetters = ''
+        secretWord = getRandomWord(words)
+        gameIsDone = False
+    else:
+        print("Thanks for playing Hangman")
